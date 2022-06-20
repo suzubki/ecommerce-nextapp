663af650a77dc1bcc97976a1d815b190a0736e48
Darwin_N19 <darwinccn25@gmail.com>
2022-06-20 18:45:08 -0500

    dinamic values

--- components/ui/ItemCounter.tsx
+++ components/ui/ItemCounter.tsx
@@ -1,15 +1,32 @@
+import { FC } from "react"
 import { Box, IconButton, Typography } from "@mui/material"
 import { RemoveCircleOutline, AddCircleOutline } from "@mui/icons-material"
-import React from "react"
 
-export const ItemCounter = () => {
+interface Props {
+  currentValue?: number
+  maxValue?: number
+
+  onUpdatedQuantity: (arg: number) => void
+}
+
+export const ItemCounter: FC<Props> = ({
+  currentValue,
+  onUpdatedQuantity,
+  maxValue = 8
+}) => {
   return (
     <Box display="flex" alignItems="center">
-      <IconButton color="primary">
+      <IconButton color="primary" onClick={() => onUpdatedQuantity(-1)}>
         <RemoveCircleOutline />
       </IconButton>
-      <Typography sx={{ width: 40, textAlign: "center" }}>1</Typography>
-      <IconButton color="primary">
+      <Typography sx={{ width: 40, textAlign: "center" }}>
+        {currentValue}
+      </Typography>
+      <IconButton
+        disabled={currentValue === maxValue && true}
+        color="primary"
+        onClick={() => onUpdatedQuantity(+1)}
+      >
         <AddCircleOutline />
       </IconButton>
     </Box>
