d85d13f4343f848f4b451df5592f399e778e026e
Darwin_N19 <darwinccn25@gmail.com>
2022-06-20 18:41:14 -0500

    chip component and state form imageLoaded for dinamic title card

--- components/products/ProductCard.tsx
+++ components/products/ProductCard.tsx
@@ -4,6 +4,7 @@ import {
   Card,
   CardActionArea,
   CardMedia,
+  Chip,
   Grid,
   Link,
   Typography
@@ -17,37 +18,52 @@ type Props = {
 
 export const ProductCard: FC<Props> = ({ product }) => {
   const [isHovered, setIsHovered] = useState(false)
+  const [isImageLoaded, setIsImageLoaded] = useState(false)
 
   const productImage = useMemo(() => {
     return isHovered
-      ? `products/${product.images[1]}`
-      : `products/${product.images[0]}`
+      ? `/products/${product.images[1]}`
+      : `/products/${product.images[0]}`
   }, [isHovered, product.images])
 
   return (
-    <Grid
-      item
-      xs={6}
-      sm={4}
-      onMouseEnter={() => setIsHovered(true)}
-      onMouseLeave={() => setIsHovered(false)}
-    >
-      <Card>
-        <NextLink href="/product/slug" passHref prefetch={false}>
+    <Grid item xs={6} sm={4}>
+      <Card
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
+      >
+        <NextLink href={`/product/${product.slug}`} passHref prefetch={false}>
           <Link>
             <CardActionArea>
+              {product.inStock === 0 && (
+                <Chip
+                  sx={{
+                    position: "absolute",
+                    zIndex: 99,
+                    top: "10px",
+                    left: "-10px",
+                    transform: "rotate(-45deg)"
+                  }}
+                  color="primary"
+                  label="Sin stock"
+                />
+              )}
               <CardMedia
                 className="fadeIn"
                 component="img"
                 image={productImage}
                 alt={product.title}
+                onLoad={() => setIsImageLoaded(true)}
               />
             </CardActionArea>
           </Link>
         </NextLink>
       </Card>
 
-      <Box sx={{ mt: 1 }} className="fadeIn">
+      <Box
+        sx={{ mt: 1, display: isImageLoaded ? "block" : "none" }}
+        className="fadeIn"
+      >
         <Typography fontWeight={700}>{product.title}</Typography>
         <Typography fontWeight={500}>${product.price}</Typography>
       </Box>
