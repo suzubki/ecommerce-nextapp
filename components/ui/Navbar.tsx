05b92d736f6f496de777bdbceb49d87f854da7f5
Darwin_N19 <darwinccn25@gmail.com>
2022-06-20 18:38:13 -0500

    navbar with dinamic products update

--- components/ui/Navbar.tsx
+++ components/ui/Navbar.tsx
@@ -1,17 +1,40 @@
+import { useContext, useState } from "react"
+import NextLink from "next/link"
+import { useRouter } from "next/router"
 import {
   AppBar,
   Badge,
   Box,
   Button,
   IconButton,
+  Input,
+  InputAdornment,
   Link,
   Toolbar,
   Typography
 } from "@mui/material"
-import { SearchOutlined, ShoppingCartOutlined } from "@mui/icons-material"
-import NextLink from "next/link"
+import {
+  ClearOutlined,
+  SearchOutlined,
+  ShoppingCartOutlined
+} from "@mui/icons-material"
+
+import { UIContext, CartContext } from "../../context"
 
 export const Navbar = () => {
+  const { asPath, push } = useRouter()
+  const { toggleSideMenu } = useContext(UIContext)
+  const { numberOfProducts } = useContext(CartContext)
+  const [searchTerm, setSearchTerm] = useState("")
+  const [isSearchVisible, setIsSearchVisible] = useState(false)
+
+  const handleClickMenu = () => toggleSideMenu()
+
+  const onSearchTerm = () => {
+    if (searchTerm.trim().length === 0) return
+    push(`/search/${searchTerm}`)
+  }
+
   return (
     <AppBar position="static">
       <Toolbar>
@@ -24,41 +47,86 @@ export const Navbar = () => {
 
         <Box flex={1} />
 
-        <Box sx={{ display: { xs: "none", sm: "block" } }}>
+        <Box
+          sx={{
+            display: isSearchVisible ? "none" : { xs: "none", sm: "block" }
+          }}
+        >
           <NextLink href="/category/men" passHref>
             <Link>
-              <Button>Hombres</Button>
+              <Button color={asPath === "/category/men" ? "primary" : "info"}>
+                Hombres
+              </Button>
             </Link>
           </NextLink>
           <NextLink href="/category/women" passHref>
             <Link>
-              <Button>Mujeres</Button>
+              <Button color={asPath === "/category/women" ? "primary" : "info"}>
+                Mujeres
+              </Button>
             </Link>
           </NextLink>
           <NextLink href="/category/kid" passHref>
             <Link>
-              <Button>Niños</Button>
+              <Button color={asPath === "/category/kid" ? "primary" : "info"}>
+                Niños
+              </Button>
             </Link>
           </NextLink>
         </Box>
 
         <Box flex={1} />
 
-        <IconButton>
+        {isSearchVisible ? (
+          <Input
+            sx={{ display: { xs: "none", sm: "flex" } }}
+            className="fadeIn"
+            autoFocus
+            value={searchTerm}
+            onChange={e => setSearchTerm(e.target.value)}
+            onKeyPress={e => (e.key === "Enter" ? onSearchTerm() : null)}
+            type="text"
+            placeholder="Buscar..."
+            endAdornment={
+              <InputAdornment position="end">
+                <IconButton onClick={() => setIsSearchVisible(false)}>
+                  <ClearOutlined />
+                </IconButton>
+              </InputAdornment>
+            }
+          />
+        ) : (
+          <IconButton
+            onClick={() => setIsSearchVisible(true)}
+            className="fadeIn"
+            sx={{ display: { xs: "none", sm: "flex" } }}
+          >
+            <SearchOutlined />
+          </IconButton>
+        )}
+
+        {/* Pantallas pequeñas */}
+        <IconButton
+          sx={{ display: { xs: "flex", sm: "none" } }}
+          onClick={toggleSideMenu}
+        >
           <SearchOutlined />
         </IconButton>
 
         <NextLink href="/cart" passHref>
           <Link>
             <IconButton>
-              <Badge>
+              <Badge
+                badgeContent={numberOfProducts > 9 ? "+9" : numberOfProducts}
+                color="secondary"
+              >
                 <ShoppingCartOutlined />
               </Badge>
             </IconButton>
           </Link>
         </NextLink>
 
-        <Button>Menu</Button>
+        <Button onClick={handleClickMenu}>Menu</Button>
       </Toolbar>
     </AppBar>
   )
