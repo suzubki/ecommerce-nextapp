4b026e2383856eabf3cbf46c86aedec44325d49f
Darwin_N19 <darwinccn25@gmail.com>
2022-06-20 18:40:01 -0500

    ordersummary dinamic, consume a context from cartcontext

--- components/cart/OrderSummary.tsx
+++ components/cart/OrderSummary.tsx
@@ -1,25 +1,34 @@
 import { Grid, Typography } from "@mui/material"
+import { useContext } from "react"
+import { CartContext } from "../../context"
+import { currency } from "../../utils"
 
 export const OrderSummary = () => {
+  const { numberOfProducts, subTotal, total, taxRate } = useContext(CartContext)
+
   return (
     <Grid container sx={{ marginTop: 1 }}>
       <Grid item xs={6}>
-        <Typography>No. Productos:</Typography>
+        <Typography>No. Productos</Typography>
       </Grid>
       <Grid item xs={6} display="flex" justifyContent="end">
-        <Typography>3</Typography>
+        <Typography>
+          {numberOfProducts} {numberOfProducts > 1 ? "productos" : "producto"}
+        </Typography>
       </Grid>
       <Grid item xs={6}>
-        <Typography>Subtotal:</Typography>
+        <Typography>Subtotal</Typography>
       </Grid>
       <Grid item xs={6} display="flex" justifyContent="end">
-        <Typography>$155.36</Typography>
+        <Typography>{currency.format(subTotal)}</Typography>
       </Grid>
       <Grid item xs={6}>
-        <Typography>Impuestos:</Typography>
+        <Typography>
+          Impuestos({Number(process.env.NEXT_PUBLIC_TAX_RATE) * 100}%)
+        </Typography>
       </Grid>
       <Grid item xs={6} display="flex" justifyContent="end">
-        <Typography>15%</Typography>
+        <Typography>{currency.format(taxRate * subTotal)}</Typography>
       </Grid>
       <Grid item xs={6} sx={{ marginTop: 2 }}>
         <Typography variant="subtitle1">Total:</Typography>
@@ -31,7 +40,7 @@ export const OrderSummary = () => {
         display="flex"
         justifyContent="end"
       >
-        <Typography variant="subtitle1">$185.34</Typography>
+        <Typography variant="subtitle1">{currency.format(total)}</Typography>
       </Grid>
     </Grid>
   )
