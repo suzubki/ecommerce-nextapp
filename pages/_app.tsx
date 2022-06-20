4cbdf46548f112f635e36ccf6b5ad9a1d05661d1
Darwin_N19 <darwinccn25@gmail.com>
2022-06-20 18:32:51 -0500

    swr context

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,29 @@
 import "../styles/globals.css"
 
 import type { AppProps } from "next/app"
+import { SWRConfig } from "swr"
 import { CssBaseline, ThemeProvider } from "@mui/material"
 
+import { CartProvider, UIProvider } from "../context"
 import { lightTheme } from "../themes"
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider theme={lightTheme}>
-      <CssBaseline />
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <SWRConfig
+      value={{
+        fetcher: (resource, init) =>
+          fetch(resource, init).then(res => res.json())
+      }}
+    >
+      <UIProvider>
+        <CartProvider>
+          <ThemeProvider theme={lightTheme}>
+            <CssBaseline />
+            <Component {...pageProps} />
+          </ThemeProvider>
+        </CartProvider>
+      </UIProvider>
+    </SWRConfig>
   )
 }
 
